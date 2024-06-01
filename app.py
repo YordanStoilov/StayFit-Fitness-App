a30eb7335226163353e4c7eabe2105c3aa3201f5
YordanStoilov <iordan27@gmail.com>
2024-06-01 19:37:54 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,191 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functions import login_required, get_exercises, User, get_exercise_id, get_token, get_needed_data_from_json, search_for_playlist
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///database.db")
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            return render_template("warning.html", message="Please fill in the forms!")
+        
+        user_data = db.execute("SELECT * FROM users WHERE username = ?", username)
+        data = user_data[0]
+
+        if not data:
+            return render_template("warning.html", message="User does not exist!")
+
+        if not check_password_hash(data["password_hash"], password) or not username == data["username"]:
+            return render_template("warning.html", message="Incorrect username or password!")
+        
+        session["user_id"] = data["user_id"]
+
+        app.logger.debug(f"Session user_id set to: {session['user_id']}")
+
+        return redirect("/")
+    
+    return render_template("index.html")
+
+@app.route("/logout", methods = ["GET", "POST"])
+@login_required
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    session.clear()
+    
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # if not email or not username or not password:
+        #     return render_template("warning.html", message="Please fill in the fields")
+
+        if db.execute("SELECT * FROM users WHERE username = ?", username):
+            return render_template("warning.html", message="Username already exists")
+        
+        db.execute("INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)", username, email, generate_password_hash(password))
+        return render_template("warning.html", message="Successfully registered!")
+
+    return render_template("register.html")
+
+@app.route("/workouts", methods=["GET", "POST"])
+@login_required
+def workouts():
+    if request.method == "POST":
+        exercise = request.form.get("exercise")
+        muscle = request.form.get("muscle")
+        difficulty = request.form.get("difficulty")
+        results_number = int(request.form.get("results_number"))
+
+        if not exercise and not muscle and not difficulty:
+            return render_template("warning.html", message="You need to input at least one value!")
+
+        results = get_exercises(exercise=exercise, muscle=muscle, difficulty=difficulty)
+
+        if not results:
+            return render_template("warning.html", message="No results.")
+
+        if not results_number:
+            results_number = 10
+
+        results = results[:results_number]
+
+        results = get_exercise_id(results)
+
+        return render_template("workout_results.html", results=results)
+    
+    return render_template("workouts.html")
+
+
+@app.route("/add_favourite", methods=["POST", "GET"])
+@login_required
+def add_favourite():
+    user_id = session["user_id"]
+
+    if request.method == "POST":
+        if request.method == "POST":
+            exercise_id = request.form.get('exercise_id')
+            
+            
+            if not db.execute("SELECT * FROM favourites WHERE exercise_id = ?", exercise_id):
+                db.execute("INSERT INTO favourites (?, ?, ?, ?, ?, ?)", user_id, )
+
+@app.route("/health_test", methods=["GET", "POST"])
+@login_required
+def health_test():
+    if request.method == "POST":
+        name = request.form.get("name")
+        age = int(request.form.get("age"))
+        weight = float(request.form.get("weight"))
+        height = int(request.form.get("height"))
+        upper_bound_bp = int(request.form.get("blood_pressure_systolic"))
+        lower_bound_bp = int(request.form.get("blood_pressure_diastolic"))
+
+        if not name or not age or not weight or not height or not upper_bound_bp or not lower_bound_bp:
+            return render_template("/warning.html", message="Please fill in all forms!")
+        
+        user = User(name, age, weight, height, (upper_bound_bp, lower_bound_bp))
+
+        user_id = session["user_id"]
+
+        if db.execute("SELECT * FROM user_vitals WHERE user_id = ?", user_id):
+            db.execute("UPDATE user_vitals SET name = ?, age = ?, weight = ?, height = ?, upper_bp = ?, lower_bp = ? WHERE user_id = ?", 
+                       name, age, weight, height, upper_bound_bp, lower_bound_bp, user_id)
+        
+        else:
+            db.execute("INSERT INTO user_vitals (user_id, name, age, weight, height, upper_bp, lower_bp) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+                    user_id, name, age, weight, height, upper_bound_bp, lower_bound_bp)
+        
+        bmi = user.get_bmi()
+        bps = user.get_blood_pressure_status()
+        max_heartrate = user.get_max_heartrate()
+        goal_pulse = user.get_goal_pulse()
+
+        if db.execute("SELECT * FROM user_results WHERE user_id = ?", user_id):
+            db.execute("UPDATE user_results SET bmi = ?, bps = ?, max_heartrate = ?, gp_upper = ?, gp_lower = ?", 
+                       bmi, bps, max_heartrate, goal_pulse[1], goal_pulse[0])
+        
+        else:
+            db.execute("INSERT INTO user_results (user_id, bmi, bps, max_heartrate, gp_upper, gp_lower) VALUES (?, ?, ?, ?, ?, ?)", 
+                       user_id, bmi, bps, max_heartrate, goal_pulse[1], goal_pulse[0])
+
+        return render_template("health_results.html", bmi=bmi, bps=bps, max_heartrate=max_heartrate, goal_pulse=goal_pulse)
+
+    return render_template("health_test.html")
+
+@app.route("/profile", methods=["POST", "GET"])
+@login_required
+def profile():
+    try:
+        user_data = db.execute("SELECT * FROM user_results WHERE user_id = ?", session["user_id"])[0]
+        user_name = db.execute("SELECT name FROM user_vitals WHERE user_id = ?", session["user_id"])[0]
+
+    except(IndexError):
+        return render_template("profile.html", user_data=[])
+
+    return render_template("profile.html", user_data=user_data, name=user_name["name"])
+
+
+@app.route("/motivated", methods=["GET", "POST"])
+@login_required
+def motivated():
+    token = get_token()
+    results = get_needed_data_from_json(search_for_playlist(token, "workout", limit=10))
+
+    return render_template("motivated.html", results=results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
---
+++ functions.py
@@ -0,0 +1,168 @@
+from functools import wraps
+from flask import redirect, session
+import requests
+import json
+from dotenv import load_dotenv
+import os
+import base64
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+# Login required decorator:
+def login_required(f):
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+# Class User to store functions about user vitals:
+class User:
+    activities_mets = {
+        "walking": 4.5,
+        "swimming": 7,
+        "jogging": 7,
+        "stretching": 4,
+        "running": 9,
+        "yoga": 3,
+        "weightlifting": 8
+    }
+
+    def __init__(self, name: str, age: int, weight: float, height: int, blood_pressure: tuple):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.blood_pressure = blood_pressure
+        self.max_heartrate = 0
+
+    def get_bmi(self):
+        height_in_m = self.height / 100
+        bmi_value = self.weight / (height_in_m ** 2)
+        return round(bmi_value, 2)
+
+    def get_blood_pressure_status(self):
+        status = ""
+        if self.blood_pressure[0] < 110 and self.blood_pressure[1] < 70:
+            status = "Low Blood Pressure"
+        elif self.blood_pressure[0] < 120 and self.blood_pressure[1] <= 80:
+            status = "Normal Blood Pressure"
+        elif 120 <= self.blood_pressure[0] < 130 and self.blood_pressure[1] <= 90:
+            status = "Elevated Blood Pressure"
+        elif 130 <= self.blood_pressure[0] < 140 and self.blood_pressure[1] <= 90:
+            status = "Pre-Hypertension"
+        elif 140 <= self.blood_pressure[0] < 160 and self.blood_pressure[1] >= 90:
+            status = "Stage 1 Hypertension"
+        elif self.blood_pressure[0] >= 160 and self.blood_pressure[1] > 100:
+            status = "Stage 2 Hypertension"
+
+        return status
+
+    def get_max_heartrate(self):
+        self.max_heartrate = 220 - self.age
+        return self.max_heartrate
+
+    def get_goal_pulse(self):
+        low_threshold = 0.5 * self.max_heartrate
+        high_threshold = 0.85 * self.max_heartrate
+        return (round(low_threshold), round(high_threshold))
+
+    def get_burned_calories(self, activity: str, minutes: int):
+        try:
+            calories_per_minute = self.activities_mets[activity.lower()] * (self.weight / 2.2) / 200
+            total_calories = round(calories_per_minute * minutes, 2)
+            return total_calories
+        
+        except KeyError:
+            return "Invalid activity!"
+        
+# Getting API data about exercises:
+def get_exercises(exercise=None, muscle=None, difficulty=None):
+
+    api_url = "https://api.api-ninjas.com/v1/exercises"
+
+    dict_inputs = {"type": exercise, "muscle": muscle, "difficulty": difficulty}
+
+    first = True
+    for key, value in dict_inputs.items():
+
+        if value is not None:
+            if first:
+                api_url += f"?{key}={value}"
+                first = False
+            else:
+                api_url += f"&{key}={value}"
+
+
+    response = requests.get(api_url, headers={'X-Api-Key': '+6tQp0+ZJwDYePUcZsXFgQ==hrcw2cmc1M6VPrPh'})
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.text)
+    else:
+        return f"Error: {response.status_code, response.text}"
+
+def get_exercise_id(results: list):
+
+    for result in results:
+        result["exercise_id"] = f'{result["name"]}&{result["type"]}&{result["muscle"]}&{result["difficulty"]}'
+
+    return results
+
+# Getting API data from Spotify:
+def get_token():
+    auth_string = f"{client_id}:{client_secret}"
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": f"Basic {auth_base64}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {"grant_type": "client_credentials"}
+    result = requests.post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+def get_auth_header(token):
+    return {"Authorization": f"Bearer {token}"}
+
+def search_for_playlist(token, genre, limit=5):
+
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={genre}&type=playlist&limit={limit}"
+    query_url = f"{url}{query}"
+
+    result = requests.get(query_url, headers=headers)
+    json_result = json.loads(result.content)["playlists"]["items"]
+
+    if len(json_result) == 0:
+        return None
+
+    return json_result
+    
+def get_needed_data_from_json(json_data):
+    necessary_data = []
+
+    for item in json_data:
+        necessary_data.append({
+        "name": item["name"], 
+        "description": item["description"], 
+        "music_link": get_spotify_embed_url(item["external_urls"]["spotify"]),
+        "image_link": item["images"][0]["url"]})
+
+    return necessary_data
+
+def get_spotify_embed_url(url):
+    left_side = url[:24]
+    right_side = url[24:]
+    return f"{left_side}/embed{right_side}"
